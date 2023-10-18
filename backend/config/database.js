fda353d4c9c93a9315c0fc974bffe5347615a279
Mohammed Moinuddin <122919446+moin532@users.noreply.github.com>
2023-10-18 23:40:03 +0700

    database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-// mongodb+srv://moinmern:moin@9606@cluster0.dcrtwb1.mongodb.net/ecomsixpack?retryWrites=true&w=majority
 //mongodb://0.0.0.0:27017/Ecommerse
 const url = process.env.DB_URI
 
